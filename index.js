5279cef691604a920191a428ed00c79839810ecd
coblmeli <coblmeli@isu.edu>
2019-10-27 21:41:33 -0600

    Recording wikipedia data to json file

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const csv = require("csv-parser");
 const fs = require("fs");
-const results = [];
+const fetch = require("node-fetch");
+const zipCodes = [];
 
-fs.createReadStream("zip-codes.csv")
-  .pipe(csv())
-  .on("data", data => results.push(data))
-  .on("end", () => {
-    console.log(results);
-  });
+const readCSV = path => {
+  fs.createReadStream(path)
+    .pipe(csv())
+    .on("data", data => zipCodes.push(data))
+    .on("end", () => {
+      zipCodes.map(async zipCode => {
+        const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=3&srsearch=${zipCode.search}`;
+        await fetch(endpoint)
+          .then(res => res.json())
+          .then(data => {
+            fs.appendFile(
+              "results.json",
+              JSON.stringify(data.query.search),
+              err => {
+                if (err) throw err;
+                console.log("Data written to file");
+              }
+            );
+          });
+      });
+    });
+};
+
+readCSV("testInputs.csv");
