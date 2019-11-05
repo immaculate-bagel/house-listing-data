301e43c45e7723a4800da72a26fd8e0f8a0e380b
coblmeli <coblmeli@isu.edu>
2019-11-05 13:33:39 -0700

    Print data from all requests

--- merge.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const csv = require("fast-csv");
-
-fs.createReadStream(".csv")
-  .pipe(csv.parse({ headers: true }))
-  .on("data", row => console.log(row));
--- print.js
+++ print.js
@@ -10,8 +10,8 @@ function sleep(ms) {
 }
 
 // A function to check if a download request has completed
-function checkDownloadUntilComplete(options, callback) {
-  const { downloadId } = options;
+function checkDownloadUntilComplete(options, callback = () => {}) {
+  const downloadId = options;
   const downloadOptions = {
     url: `https://api.datafiniti.co/v4/downloads/${downloadId}`,
     method: "GET",
@@ -21,45 +21,62 @@ function checkDownloadUntilComplete(options, callback) {
     }
   };
 
-  const end = (i, results, filename, exit) => {
-    console.log(`File ${i + 1} out of ${results.length} saved: ${filename}`);
-    if (exit) process.exit();
-  };
+  request(downloadOptions, async (error, response, body) => {
+    if (error) {
+      throw new Error(error);
+    }
 
-  request(downloadOptions, async body => {
     let numFilesDownloaded = 0;
     const downloadResponse = JSON.parse(body);
     console.log(`Records downloaded: ${downloadResponse.num_downloaded}`);
     if (downloadResponse.status !== "completed") {
-      // NEED A SLEEP FUNCTION HERE!
       await sleep(5000);
       checkDownloadUntilComplete(options, callback);
     } else {
       const { results } = downloadResponse;
       console.log(results);
       for (let i = 0; i < results.length; i++) {
-        const filename = `print_${downloadId}_${i}.${format}`;
+        const filename = `${downloadId}_${i}.${format}`;
         const file = fs.createWriteStream(filename);
         request(results[i])
           .pipe(file)
-          .on(
-            "end",
-            end(
-              i,
-              results,
-              filename,
-              (numFilesDownloaded += 1) === results.length
-            )
-          );
+          .on("end", () => {
+            numFilesDownloaded += 1;
+            if (numFilesDownloaded === results.length) {
+              process.exit();
+            }
+          });
       }
     }
   });
 }
 
-// Initiate the download request.
-request({}, () => {
-  //   const downloadId = 15151;
-  const downloadId = 15097;
-  // Check on status of the download request.
-  checkDownloadUntilComplete({ downloadId }, () => {});
-});
+const main = async () => {
+  let uniqueIds = [];
+  const downloadIds = [15150, 15151, 15157, 15158, 15159, 15160, 15096, 15097];
+  const downloadOptions = {
+    url: `https://api.datafiniti.co/v4/downloads`,
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${APIToken}`,
+      "Content-Type": "application/json"
+    }
+  };
+  await request(downloadOptions, async (error, response, body) => {
+    if (error) {
+      throw new Error(error);
+    }
+    const { records } = JSON.parse(response.body);
+    const restOfIds = records.map(r => {
+      return parseInt(r.id, 10);
+    });
+    uniqueIds = [...downloadIds, ...restOfIds]
+      .filter((x, i, a) => a.indexOf(x) === i)
+      .sort();
+    uniqueIds.map(async id => {
+      await checkDownloadUntilComplete(id);
+    });
+  });
+};
+
+main();
